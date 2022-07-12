bf144be385e383fa036a6af59807ed432712a1dc
unknown <ahmed.trifi007@gmail.comgit config --global user.name AhmedTrifi>
2022-07-12 11:04:35 +0100

    last version

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
-$('body').css('background-color', 'red')
-$('img').attr('src', 'http://i.imgur.com/nWGCwiE.gif')
+function doSomething(){
+    document.getElementById("num1").innerHTML = "hello"    
+}
